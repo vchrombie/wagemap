821686368bf52d45618a1650b9126fa40daf398b
Venu Vardhan Reddy Tekula <venuvrtekula@gmail.com>
2025-12-26 19:16:26 -0500

    Configure Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "./Map";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Map />
+      <Analytics />
     </div>
   );
 }
